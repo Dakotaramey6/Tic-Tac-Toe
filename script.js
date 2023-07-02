2de3b07f99658da394e4e8593a30289fd21f7964
Dakota <Dakotaramey6@gmail.com>
2023-07-02 07:18:17 -0400

    Win condition added

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const board = document.querySelector('.board');
 let tableData = document.querySelectorAll('.row > td');
 let OTurn = false; //Makes X Start Game
+let winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
 
 const createX = (arr) => {
   let X = document.createElement('span');
@@ -14,21 +24,30 @@ const createO = (arr) => {
   OTurn = false;
 };
 
-const checkWin = () => {
-  for (let i = 0; i < tableData.length; i++)
-    if (tableData[0].innerText === 'O') {
-      alert('ah');
+const checkWin = (gameBoard, player) => {
+  for (let i = 0; i < winConditions.length; i++) {
+    let sum = 0;
+    let win = winConditions[i];
+
+    for (let j = 0; j < win.length; j++) {
+      if (gameBoard[win[j]].innerText === player) {
+        sum++;
+        console.log(sum);
+      }
     }
+    if (sum === 3) return true;
+  }
+  return false;
 };
 
 for (let i = 0; i < tableData.length; i++) {
   tableData[i].addEventListener('click', () => {
     if (OTurn === false) {
       createX(tableData[i]);
-      checkWin();
+      checkWin(tableData, 'X');
     } else {
       createO(tableData[i]);
-      checkWin();
+      checkWin(tableData, 'O');
     }
   });
 }
